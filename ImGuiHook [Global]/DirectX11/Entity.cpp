217ec31184dc9f26625b1c80d39044bd70dd64bf
Noust <supergmerpromx@gmail.com>
2022-08-15 16:28:35 -0500

    fix the entity.cpp

--- ImGuiHook [Global]/DirectX11/Entity.cpp
+++ ImGuiHook [Global]/DirectX11/Entity.cpp
@@ -3,7 +3,7 @@
 DWORD64 Entity::GetEntity(int index) {
 	DWORD64 EntityA = *(DWORD64*)((DWORD64)GetModuleHandleA("GTA5.exe") + 0x266DBC8);
 	if (EntityA != 0)
-		return *(DWORD64*)(EntityA + ((index + 0x18) * (6 << 2)));
+		return *(DWORD64*)((EntityA + 0x18) + (index * (6 << 2)));
 	return 0;
 }
 short Entity::GetMaxEntities() {
