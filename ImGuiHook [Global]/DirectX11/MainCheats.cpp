b29ed28e10e2259fd08c7e7dca138a8230ad1e04
Noust <supergmerpromx@gmail.com>
2022-08-17 16:36:29 -0500

    all working realese

--- ImGuiHook [Global]/DirectX11/MainCheats.cpp
+++ ImGuiHook [Global]/DirectX11/MainCheats.cpp
@@ -8,82 +8,84 @@ DWORD WINAPI SetValues(HMODULE hMod) {
 		if (hooked) {
 			local = (Entitys*)(E.GetLocalPlayer());
 			if (local != 0 && !OnPause()) {
-				if (UserSettings.MenuWindow != 2) {
-					if (ShowMenu) {
-						if (UserSettings.Godmode) {
-							if (local->GodMode != true)
-								local->GodMode = true;
+				if (ShowMenu && UserSettings.MenuWindow != 2) {
+					if (UserSettings.Godmode) {
+						if (local->GodMode != true)
+							local->GodMode = true;
+					}
+					else {
+						if (local->GodMode != false)
+							local->GodMode = false;
+					}
+					if (UserSettings.CarGodMode) {
+						if (E.IsInCar()) {
+							if (local->CarPtr->GodMode != true)
+								local->CarPtr->GodMode = true;
 						}
-						else {
-							if (local->GodMode != false)
-								local->GodMode = false;
+					}
+					else {
+						if (E.IsInCar()) {
+							if (local->CarPtr->GodMode != false)
+								local->CarPtr->GodMode = false;
 						}
-						if (UserSettings.CarGodMode) {
-							if (E.IsInCar()) {
-								if (local->CarPtr->GodMode != true)
-									local->CarPtr->GodMode = true;
-							}
-						}
-						else {
-							if (E.IsInCar()) {
-								if (local->CarPtr->GodMode != false)
-									local->CarPtr->GodMode = false;
-							}
-						}
-						if (UserSettings.nocarcollision) {
-							if (E.IsInCar()) {
+					}
+					if (UserSettings.nocarcollision) {
+						if (E.IsInCar()) {
+							if (local->CarPtr->CarCustom != 0) {
 								if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 0)
 									local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 0;
 							}
 						}
-						else {
-							if (E.IsInCar()) {
+					}
+					else {
+						if (E.IsInCar()) {
+							if (local->CarPtr->CarCustom != 0) {
 								if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 1)
 									local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 1;
 							}
 						}
-						if (UserSettings.InfAmmo) {
-							if (!initiate) {
-								Patch((BYTE*)PatchAddr, (BYTE*)"\x90\x90\x90\x90\x90", 5);
-								Patch((BYTE*)PatchAddr1, (BYTE*)"\x90\x90\x90", 3);
-								initiate = true;
-								initiate1 = false;
-							}
+					}
+					if (UserSettings.InfAmmo) {
+						if (!initiate) {
+							Patch((BYTE*)PatchAddr, (BYTE*)"\x90\x90\x90\x90\x90", 5);
+							Patch((BYTE*)PatchAddr1, (BYTE*)"\x90\x90\x90", 3);
+							initiate = true;
+							initiate1 = false;
 						}
-						else {
-							if (!initiate1) {
-								Patch((BYTE*)PatchAddr, (BYTE*)"\x66\x45\x89\x53\x56", 5);
-								Patch((BYTE*)PatchAddr1, (BYTE*)"\x89\x5F\x20", 3);
-								initiate1 = true;
-								initiate = false;
-							}
+					}
+					else {
+						if (!initiate1) {
+							Patch((BYTE*)PatchAddr, (BYTE*)"\x66\x45\x89\x53\x56", 5);
+							Patch((BYTE*)PatchAddr1, (BYTE*)"\x89\x5F\x20", 3);
+							initiate1 = true;
+							initiate = false;
 						}
-						if (UserSettings.KillAura) {
-							for (int i = 0; i < E.GetMaxEntities(); i++) {
-								DWORD64 EntityAddr = E.GetEntity(i);
-								if (EntityAddr != 0) {
-									entsK = (Entitys*)(EntityAddr);
-									if (local->pos.Distance(entsK->pos) < UserSettings.KillAuraDist) {
-										entsK->Health = 0;
-									}
-								}
-							}
-						}
-						if (UserSettings.CustomValues) {
-							if (E.Alive()) {
-								local->PlayerInfoPtr->SwimSpeed = UserSettings.SwimSpeed;
-								local->PlayerInfoPtr->RunSpeed = UserSettings.runSpeed;
-								if (E.IsInCar()) {
-									local->CarPtr->CarCustom->ACCELERATION = UserSettings.caracceleration;
-									local->CarPtr->GravityCar = UserSettings.carGravity;
+					}
+					if (UserSettings.KillAura) {
+						for (int i = 0; i < E.GetMaxEntities(); i++) {
+							DWORD64 EntityAddr = E.GetEntity(i);
+							if (EntityAddr != 0) {
+								entsK = (Entitys*)(EntityAddr);
+								if (local->pos.Distance(entsK->pos) < UserSettings.KillAuraDist) {
+									entsK->Health = 0;
 								}
 							}
 						}
 					}
-					if (UserSettings.NeverWanted) {
-						if (local->PlayerInfoPtr->WantedLevel = 0)
-							local->PlayerInfoPtr->WantedLevel = 0;
+					if (UserSettings.CustomValues) {
+						if (E.Alive()) {
+							local->PlayerInfoPtr->SwimSpeed = UserSettings.SwimSpeed;
+							local->PlayerInfoPtr->RunSpeed = UserSettings.runSpeed;
+							if (E.IsInCar() && local->CarPtr->CarCustom != 0) {
+								local->CarPtr->CarCustom->ACCELERATION = UserSettings.caracceleration;
+								local->CarPtr->GravityCar = UserSettings.carGravity;
+							}
+						}
 					}
+				}
+				if (UserSettings.NeverWanted && !ShowMenu && local->PlayerInfoPtr != 0) {
+					if (local->PlayerInfoPtr->WantedLevel = 0)
+						local->PlayerInfoPtr->WantedLevel = 0;
 				}
 			}
 		}
