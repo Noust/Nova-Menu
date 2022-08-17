1a2d51e695697abac1590dd961106fa99bfe1e26
Noust <supergmerpromx@gmail.com>
2022-08-16 21:29:20 -0500

    all fixed

--- ImGuiHook [Global]/DirectX11/MainCheats.cpp
+++ ImGuiHook [Global]/DirectX11/MainCheats.cpp
@@ -1,47 +1,83 @@
 #include "Include.h"
 bool initiate = false;
 bool initiate1 = false;
+bool initiate2 = false;
+bool initiate3 = false;
+bool initiate4 = false;
+bool initiate5 = false;
+bool initiate6 = false;
+bool initiate7 = false;
 
 DWORD WINAPI SetValues(HMODULE hMod) {
 	Entity E;
-	while (!GetAsyncKeyState(VK_NUMPAD1)) {
+	while (!GetAsyncKeyState(VK_DELETE)) {
 		if (hooked) {
 			local = (Entitys*)(E.GetLocalPlayer());
 			if (local != 0) {
 				if (UserSettings.Godmode) {
-					if (E.Alive()) {
-						if (local->GodMode != true)
-							local->GodMode = true;
+					if (!initiate2) {
+						if (E.Alive()) {
+							if (local->GodMode != true) {
+								local->GodMode = true;
+								initiate2 = true;
+								initiate3 = false;
+							}
+						}
 					}
 				}
 				else {
-					if (E.Alive()) {
-						if (local->GodMode != false)
-							local->GodMode = false;
+					if (!initiate3) {
+						if (E.Alive()) {
+							if (local->GodMode != false) {
+								local->GodMode = false;
+								initiate2 = false;
+								initiate3 = true;
+							}
+						}
 					}
 				}
 				if (UserSettings.CarGodMode) {
-					if (E.CarExist()) {
-						if (local->CarPtr->GodMode != true)
-							local->CarPtr->GodMode = true;
+					if (!initiate4) {
+						if (E.CarExist()) {
+							if (local->CarPtr->GodMode != true) {
+								local->CarPtr->GodMode = true;
+								initiate4 = true;
+								initiate5 = false;
+							}
+						}
 					}
 				}
 				else {
-					if (E.CarExist()) {
-						if (local->CarPtr->GodMode != false)
-							local->CarPtr->GodMode = false;
+					if (!initiate5) {
+						if (E.CarExist()) {
+							if (local->CarPtr->GodMode != false) {
+								local->CarPtr->GodMode = false;
+								initiate4 = false;
+								initiate5 = true;
+							}
+						}
 					}
 				}
 				if (UserSettings.nocarcollision) {
-					if (E.CarExist()) {
-						if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 0)
-							local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 0;
+					if (!initiate6) {
+						if (E.CarExist()) {
+							if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 0) {
+								local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 0;
+								initiate6 = true;
+								initiate7 = false;
+							}
+						}
 					}
 				}
 				else {
-					if (E.CarExist()) {
-						if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 1)
-							local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 1;
+					if (!initiate7) {
+						if (E.CarExist()) {
+							if (local->CarPtr->CarCustom->DEFORM_MULTIPLIER != 1) {
+								local->CarPtr->CarCustom->DEFORM_MULTIPLIER = 1;
+								initiate6 = false;
+								initiate7 = true;
+							}
+						}
 					}
 				}
 				if (UserSettings.NeverWanted) {
@@ -51,7 +87,7 @@ DWORD WINAPI SetValues(HMODULE hMod) {
 					}
 				}
 				if (UserSettings.InfAmmo) {
-					while (!initiate) {
+					if (!initiate) {
 						Patch((BYTE*)PatchAddr, (BYTE*)"\x90\x90\x90\x90\x90", 5);
 						Patch((BYTE*)PatchAddr1, (BYTE*)"\x90\x90\x90", 3);
 						initiate = true;
@@ -59,7 +95,7 @@ DWORD WINAPI SetValues(HMODULE hMod) {
 					}
 				}
 				else {
-					while (!initiate1) {
+					if (!initiate1) {
 						Patch((BYTE*)PatchAddr, (BYTE*)"\x66\x45\x89\x53\x56", 5);
 						Patch((BYTE*)PatchAddr1, (BYTE*)"\x89\x5F\x20", 3);
 						initiate1 = true;
